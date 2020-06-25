924fb8ad6fc2a7ee9d5c4869629ed82bfeaacf89
aryanagarwal <aryanagarwal001@outlook.com>
2020-06-24 21:46:21 -0500

    fixed wdf flipping, deprecated old files

--- utils/importer.py
+++ utils/importer.py
@@ -16,6 +16,7 @@ def read(filename):
         data = hs.load("temp.png")
         os.remove("temp.png")
         return data
+    '''
     if (extension == "txt"):
         raw = np.genfromtxt(filename, delimiter="\t", skip_header=1, dtype=float, names =("X","Y","Wavelength","Intensity"))
         #raw["X"] *= -1
@@ -24,7 +25,16 @@ def read(filename):
         wavesize = int(np.shape(raw)[0]/xCoords/yCoords)
         raw = np.sort(raw, axis=-1, order = ("Y","X","Wavelength"))
         raw = (raw["Intensity"].reshape((xCoords, yCoords, wavesize)))
-        return hs.signals.Signal1D(raw)
+        data = hs.signals.Signal1D(raw)
+        data.metadata.General.title = filename.split('.',1)[0]
+        data.axes_manager[0].name = 'x'
+        data.axes_manager[1].name = 'y'
+        data.axes_manager[2].name = 'wavelength'
+        data.axes_manager[0].units = "um"
+        data.axes_manager[1].units = "um"
+        data.axes_manager[2].units = "1/cm"
+        return data
+    '''
     if (extension in loadables["extension"]):
         if (loadables["extension" == extension]["signal"] == ""):
             return hs.load(filename, signal_type=loadables["extension" == extension]["signal"])
@@ -32,9 +42,16 @@ def read(filename):
             return hs.load(filename)
     if (extension == "wdf"):
         reader = WDFReader(filename)
-        #reader.print_info()
+        reader.spectra = np.flip(reader.spectra,axis=2)
         data = hs.signals.Signal1D(reader.spectra)
         data.metadata.General.title = reader.title
+        data.metadata.General.authors = reader.username
+        data.axes_manager[0].name = 'x'
+        data.axes_manager[1].name = 'y'
+        data.axes_manager[2].name = 'wavelength'
+        data.axes_manager[0].units = str(reader.xpos_unit).replace('μ', 'u')
+        data.axes_manager[1].units = str(reader.ypos_unit).replace('μ', 'u')
+        data.axes_manager[2].units = str(reader.xlist_unit)
         return data
     else:
         print("INVALID FILE TYPE")
